d835fae03fff9e851cb8dea01c25ec88fa73a611
Fabian Senf <senf@tropos.de>
2025-05-15 18:31:04 +0200

    extended feature combiner such that daily combination are possible

--- scripts/05-Combine_Features.py
+++ scripts/05-Combine_Features.py
@@ -35,7 +35,7 @@ import json
 
 if 'launcher' in sys.argv[0]:
     config_file = '../scripts/config/config_precip500.json'
-    date = '202006'
+    date = '20200601'
     interactive = True
 else:
     config_file = sys.argv[1]
@@ -55,7 +55,7 @@ globals().update(conf)
 
 # ## Input Data 
 
-# In[9]:
+# In[4]:
 
 
 main_dir = "/work/bb1376/user/fabian/data/oxford-hackathon"
@@ -64,10 +64,11 @@ subname = f"{vname}_features_thresh{threshold}"
 input_dir = f"/{main_dir}/{model}/{experiment}/{subname}"
 
 #precip_features_thresh50_icon-hamlite_20200630T0045.nc
-filelist = sorted( glob.glob( f'{input_dir}/{subname}*_{date}??T????.nc' ))
+
+filelist = sorted( glob.glob( f'{input_dir}/{subname}*_{date}*T????.nc' ))
 
 
-# In[10]:
+# In[5]:
 
 
 feature_list = []
@@ -77,12 +78,12 @@ for fname in filelist:
     print(f'...open {fname}')
     xfeat = xr.open_dataset( fname )
     features = xfeat.to_pandas()
-
+    
     feature_list += [ features.copy(), ]
+    
 
 
-
-# In[11]:
+# In[6]:
 
 
 features = tobac.utils.general.combine_feature_dataframes( feature_list )
@@ -91,32 +92,32 @@ xfeat = features.to_xarray()
 
 # ## Plotting 
 
-# In[ ]:
+# In[7]:
 
 
 if interactive:
-
+    
     size = np.sqrt( xfeat['area'].data / 1e6 )
     mag = xfeat['max'].data
-
+    
     h, xe, p = plt.hist( mag, 10, log = True)
 
 
-# In[19]:
+# In[8]:
 
 
 if interactive:
     target_map = ccrs.PlateCarree()
     fig, axs = plt.subplots(ncols=1, nrows = 1, subplot_kw={'projection': target_map}, figsize = (24,4))
     plt.sca( axs ) #[0] )
-
+    
     x = xfeat.longitude.data
     y = xfeat.latitude.data
-
+    
     for i  in range( len(xe) - 1):
         v1 = xe[i]
         v2 = xe[i+1]
-
+        
         m = (mag > v1) & (mag <= v2 )
         unit = 'mm day${}^{-1}$'
         plt.scatter( x[m],y[m], c = mag[m], s = np.log(size[m]), cmap = plt.cm.Spectral_r, 
@@ -125,20 +126,29 @@ if interactive:
 #    xfeat.plot.scatter( x = 'longitude', y = 'latitude', markersize =size, 
 #                       transform=ccrs.PlateCarree(),  # the data's projection
 #            subplot_kws={"projection": target_map},)
-
+    
     ax = plt.gca()
     ax.add_feature(cf.COASTLINE, linewidth=0.8)
     ax.add_feature(cf.BORDERS, linewidth=0.4)
     ax.set_title(f'Distribution of precip. cells (> 50 {unit}) for {date}', fontweight = 'bold') 
-
+    
     # plt.savefig(f'../pics/map_precip_cells_thresh50_{date}.jpg')
 
 
 # ## Output 
 
-# In[12]:
+# In[9]:
 
 
-outfile = f'{input_dir}/monthly_{subname}_{date}.nc'
+if len(date) == 6:
+    aggregate = 'monthly'
+elif len(date) == 8:
+    aggregate = 'daily'
+
+
+# In[10]:
+
+
+outfile = f'{input_dir}/{aggregate}_{subname}_{date}.nc'
 xfeat.to_netcdf( outfile )
 
